ccb5d797a32675839bab5fe82579ed990c1e4a73
jared-allison <47228513+jared-allison@users.noreply.github.com>
2019-03-29 15:09:45 -0500

    Find Biggest Lego Set

---
+++ scrapy_demo.py
@@ -0,0 +1,90 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+import urllib.request
+from PIL import Image
+
+class BrickSetSpider(scrapy.Spider):
+    name = 'brick_spider'
+    start_urls = ['http://brickset.com/sets/year-1949']
+    image_URL = []
+    piece_count = []
+    name_count = []
+    year_count = []
+#    counter = 0
+    
+    def parse(self, response):
+        TITLE_SELECTOR = 'title ::text'
+        SET_SELECTOR = '.set'
+        for brickset in response.css(SET_SELECTOR):
+            
+            NAME_SELECTOR = 'h1 ::text'
+            PIECES_SELECTOR = './/dl[dt/text() = "Pieces"]/dd/a/text()'
+            MINIFIGS_SELECTOR = './/dl[dt/text() = "Minifigs"]/dd[2]/a/text()'
+            IMAGE_SELECTOR = 'img ::attr(src)'
+            yield {
+                'title': response.css('.no-js').css(TITLE_SELECTOR).extract_first().split()[0],
+                'name': brickset.css(NAME_SELECTOR).extract_first(),
+                'pieces': brickset.xpath(PIECES_SELECTOR).extract_first(),
+                'minifigs': brickset.xpath(MINIFIGS_SELECTOR).extract_first(),
+                'image': brickset.css(IMAGE_SELECTOR).extract_first(),
+            }            
+            self.year_count.append(response.css('.no-js').css(TITLE_SELECTOR).extract_first().split()[0])
+            self.image_URL.append(brickset.css(IMAGE_SELECTOR).extract_first())
+            self.piece_count.append(brickset.xpath(PIECES_SELECTOR).extract_first())
+            self.name_count.append(brickset.css(NAME_SELECTOR).extract_first())
+#            self.counter = self.counter+1
+#            if self.counter > 25:
+#                return
+        NEXT_PAGE_SELECTOR = '.next a ::attr(href)'
+        next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
+        if next_page:
+            yield scrapy.Request(
+                response.urljoin(next_page),
+                callback=self.parse
+            )
+        else: # If there is not a next page, then go on to the next year
+            NEXT_YEAR_SELECTOR = '.col a ::attr(href)'
+            next_year = response.css(NEXT_YEAR_SELECTOR).extract()[-1]
+            if next_year:
+                yield scrapy.Request(
+                    response.urljoin(next_year),
+                    callback=self.parse
+                )
+            
+def is_int(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+    
+if __name__ == "__main__":
+    process = CrawlerProcess()
+    process.crawl(BrickSetSpider)
+    process.start() # the script will block here until the crawling is finished
+    images = BrickSetSpider.image_URL
+    names = BrickSetSpider.name_count
+    pieces = BrickSetSpider.piece_count
+    year = BrickSetSpider.year_count
+    
+    maxPiece = 0
+    maxind = 0
+    i = 0
+    for piece in pieces:
+        if piece is not None:
+            if is_int(piece):                 
+                if int(piece) > maxPiece:
+                    maxPiece = int(piece)
+                    maxind = i
+        i = i+1
+    
+    maxName = names[maxind]
+    maxImg = images[maxind]
+    
+    print()
+    print(maxName,maxPiece,maxImg,year[maxind])
+    
+    urllib.request.urlretrieve(maxImg,'Max_Image.png')
+    img = Image.open('Max_Image.png')
+    img.show()
+    
