3cd996912d469d7ebc3e79e49883dc70cdf48dbd
georginad <42850648+georginad@users.noreply.github.com>
2019-04-18 16:48:28 -0500

    some ics in utfeeder and code to find if rss feed is in webpage

--- utFeeder.py
+++ utFeeder.py
@@ -1,6 +1,101 @@
 import feedparser
 import re
+import urllib.request
+import requests
+from bs4 import BeautifulSoup as bs4
 
+#add function that checks for if link provided has rss feed and returns the link to the feed
+
+def extractICS(a,extract):
+    
+    '''
+    extracts certain lines from the ics file. extract must be a keyword string examples:
+        DTEND for end time
+        DTSTART for start time
+        LOCATION for building name and room number
+    '''
+    
+    a = a.split('\n')
+    for itm in a:
+        if itm.find(extract)>=0:
+            return itm
+        
+def _get_ics(link):
+    
+    '''
+    get ics file from the link in the event
+        ics on the link has all the times, so parse the ics file so that
+        you only get the time event that matches the extracted time in the event
+    '''
+    
+    #need to fix this so that it only get the time that matters
+    url = link
+    urllist = [url,'.ics']
+    url = ''.join(urllist)
+    urllib.request.urlretrieve(url,'temp.ics')
+    
+    #clean up the stuff in the ics file
+    with open('temp.ics', encoding = "utf8", mode = 'r') as myfile:
+        myICS = myfile.read()
+    
+    myICS = myICS.split('BEGIN')
+    myICS = 'BEGIN'.join(myICS[0:3])
+    
+    return myICS
+
+def getICSprops(event):
+    
+    '''
+        takes in an event eg: test.foodEs[0] and gets the time (start and end) as well as the building and location
+        from the ics file
+    '''
+    
+    ICSstr = _get_ics(event.link)
+    event.timestart = extractICS(ICSstr,'DTSTART')
+    event.timeend = extractICS(ICSstr,'DTEND')
+    event.timestamp = extractICS(ICSstr,'DTSTAMP')
+    event.locwithBUI = extractICS(ICSstr,'LOCATION') #location with building number
+    event.category = extractICS(ICSstr,'CATEGORIES') #category of event
+    event.summary = extractICS(ICSstr,'SUMMARY')
+
+def addtoICS(event,start,oldstring = ''):
+    
+    '''
+        Create ICS string for file in the format to open up in calendar
+    '''
+    #add name -> need to parse out the description so that it only gets valid stuff
+    #name is summart
+    fillme = []
+    
+    if start == 1 and len(oldstring)==0: #yes starting a new calendar
+        fillme.append('BEGIN:VCALENDAR\nVERSION:2.0\nCALSCALE:GREGORIAN\nPRODID:iCalendar-Ruby\n')
+        
+    fillme.append('BEGIN:VEVENT\n')
+    
+    if event.category: fillme.append(event.category+'\n')
+    if event.description: fillme.append('DESCRIPTION:'+event.description+'\n')
+    if event.timeend: fillme.append(event.timeend+'\n')
+    if event.timestart: fillme.append(event.timestart+'\n')
+    if event.timestamp: fillme.append(event.timestamp+'\n')
+    if event.locwithBUI: fillme.append(event.locwithBUI+'\n')
+    if event.summary: fillme.append(event.summary+'\n')
+    
+    fillme.append('END:VEVENT\n')
+    
+    newstring = ''.join(fillme)
+    
+    return oldstring+newstring
+
+def writetoICS(string,filename='Food_Calendar.ics'):
+    
+    '''
+        Write string to fileName
+    '''
+    
+    with open(filename, encoding = "utf8", mode = 'w') as myfile:
+        myfile.write(string)    
+    
+    
 class foodE:
     ''' Food event generated from calendar feed'''
 
@@ -29,10 +124,7 @@ class foodE:
             if hasattr(self, name):
                 value = (value, getattr(self, name))
             setattr(self, name, value)
-            
-    def _get_ics(self):
-        print('idk')
-        
+                
 class Feeder:
     ''' Interact with RSS feed '''
     
@@ -50,7 +142,7 @@ class Feeder:
                 print('no updates')
             else:
                 self.feed = feed_temp
-                self.make_foodEs
+                self.make_foodEs()
         else:
             print('no etag')
             self.feed = feedparser.parse(url)
