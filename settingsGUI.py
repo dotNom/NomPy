2151623c8e82fc56c7f5d4964431e451a49553cf
georginad <42850648+georginad@users.noreply.github.com>
2019-04-27 23:52:23 -0500

    Added scrollbar for food and url lists

--- settingsGUI.py
+++ settingsGUI.py
@@ -19,74 +19,87 @@ class setGUI:
                  text = "CURRENT CALENDAR OUTPUT:",
                  fg = "white",
                  bg = "red",
-                 font = ('Georgia','24','bold')).pack()        
+                 font = ('Georgia','24','bold')).grid(row = 0, column = 0)        
         self.calendar = tk.Label(self.launch,
                  text = '\nYES Calendar ==> .ics file will be saved\n',
                  fg = "red",
                  font = ('Georgia','14','italic'))
-        self.calendar.pack()
+        self.calendar.grid(row = 1, column = 0)
         
-        tk.Button(self.launch,text = "CHANGE CALENDAR OPTION", command = self.tkYesNo).pack()
-        
-        tk.Label(self.launch,text="\n").pack() #empty line
+        tk.Button(self.launch,text = "CHANGE CALENDAR OPTION", command = self.tkYesNo).grid(row = 2, column = 0)
         
         #Ask to add food options, and display list of options
         tk.Label(self.launch,
                  text = "CURRENT FOOD OPTIONS:",
                  fg = "white",
                  bg = "red",
-                 font = ('Georgia','24','bold')).pack()
-        tk.Label(self.launch,text="").pack() #empty line
-        self.foodList = tk.Label(self.launch,
-                 text = listTostr(self.foodToSearch,', '),
-                 fg = "red",
-                 font = ('Georgia','14','italic'))
-        self.foodList.pack()
+                 font = ('Georgia','24','bold')).grid(row = 4, column = 0)
+        
+        scrollfood = tk.Scrollbar(self.launch)
+        scrollfood.grid(row = 5, column = 50, sticky='ns')
+        self.foodlist = tk.Listbox(self.launch, 
+                                   width =  40, height = 5,
+                                   yscrollcommand = scrollfood.set)
+        
+        #initial foodlist
+        for fd in self.foodToSearch:
+            self.foodlist.insert(tk.END,fd)
+        
+        self.foodlist.config(fg = "red", font = ('Georgia','14','italic'))
+        self.foodlist.grid(row = 5, column = 0)
+        
+        scrollfood.config(command=self.foodlist.yview)
+
         tk.Label(self.launch,
                  text = "\nAdd more food options to search from:\n",
                  fg = "black",
-                 font = ('Georgia','12')).pack()
+                 font = ('Georgia','12')).grid(row = 11, column = 0)
                
-        tk.Button(self.launch,text = "ADD FOOD",command = self.dialfood).pack()
-        
-        tk.Label(self.launch,text="\n").pack() #empty line
+        tk.Button(self.launch,text = "ADD FOOD",command = self.dialfood).grid(row = 12, column = 0)
         
         #Ask to add the url links and include a check to make sure that the link contains a valid rss feed (.xml or /rss)
         tk.Label(self.launch,
                  text = "CURRENT URL OPTIONS:",
                  fg = "white",
                  bg = "red",
-                 font = ('Georgia','24','bold')).pack() 
-        tk.Label(self.launch,text="").pack() #empty line
-        self.url = tk.Label(self.launch,
-                 text = listTostr(self.URLlist,'\n'),
-                 fg = "red",
-                 font = ('Georgia','14','italic'))
-        self.url.pack()
+                 font = ('Georgia','24','bold')).grid(row = 13, column = 0) 
+        
+        scrollurl = tk.Scrollbar(self.launch)
+        scrollurl.grid(row = 14, column = 65, sticky='ns')
+        self.url = tk.Listbox(self.launch, 
+                              width =  60, height = 5,
+                              yscrollcommand = scrollurl.set)
+        
+        #initial urllist
+        for ul in self.URLlist:
+            self.url.insert(tk.END,ul)
+        
+        self.url.config(fg = "red", font = ('Georgia','14','italic'))
+        self.url.grid(row = 14, column = 0)
+        
+        scrollfood.config(command=self.url.yview)
+        
         tk.Label(self.launch,
                  text = "\nAdd more url options to search from:\n",
                  fg = "black",
-                 font = ('Georgia','12')).pack()
+                 font = ('Georgia','12')).grid(row = 20, column = 0)
         
-        tk.Button(self.launch,text = "ADD URL",command = self.dialurl).pack()
+        tk.Button(self.launch,text = "ADD URL",command = self.dialurl).grid(row = 21, column = 0)
         tk.Label(self.launch,
                  text = "\nOr select from loaded defaults:\n",
                  fg = "black",
-                 font = ('Georgia','12')).pack()
-        tk.Button(self.launch,text = "LOAD DEFAULT URLS",command = self.defaulturls).pack()
-        
-        tk.Label(self.launch,text="\n").pack() #empty line
+                 font = ('Georgia','12')).grid(row=22,column = 0)
+        tk.Button(self.launch,text = "LOAD DEFAULT URLS",command = self.defaulturls).grid(row = 23, column = 0)
         
         #Exit when done
-        tk.Button(self.launch,text = "DONE",command = self.done).pack()
+        tk.Button(self.launch,text = "DONE",command = self.done).grid(row = 24, column = 0)
         
         self.launch.mainloop()
         
     def dialfood(self):
         newfood = simpledialog.askstring("More food","Please Enter food.",parent=self.launch)
         addtolist(newfood,self.foodToSearch)
-        self.foodList['text'] = listTostr(self.foodToSearch,', ')
-    
+        self.foodlist.insert(tk.END,newfood)    
     
     def dialurl(self):
         newurl = simpledialog.askstring("More links","Please Enter URL.",parent=self.launch)
@@ -99,12 +112,12 @@ class setGUI:
             
             if urlOK:
                 addtolist(newurl,self.URLlist)
-                self.url['text'] = listTostr(self.URLlist,'\n')  
+                self.url.insert(tk.END,newurl) 
             else:
                 self.tkInvalid()
     
     def dialdel(self):
-        torem = simpledialog.askinteger("delete url","Please Enter index of URL to remove.\nIndices are given in the LOADED DEFAULTS list by the URL.",parent=self.launch,minvalue=0,maxvalue=len(self.defaults)-1) 
+        torem = simpledialog.askinteger("delete url","Please Enter index of URL to remove.\nIndices are given in the LOADED DEFAULTS list by the URL.",parent=self.deflt,minvalue=0,maxvalue=len(self.defaults)-1) 
         if torem in range(len(self.defaults)): del(self.defaults[torem])
         self.defList['text'] = listTostr(self.defaults,'\n',item=1)
                 
@@ -128,7 +141,7 @@ class setGUI:
         self.URLlist = []
         for url in self.defaults:
             addtolist(url,self.URLlist)
-        self.url['text'] = listTostr(self.URLlist,'\n')  
+            self.url.insert(tk.END, url) 
         self.deflt.destroy()
         
     def tkYesNo(self):
@@ -193,3 +206,11 @@ def listTostr(foodIn,sep,item=0):
 def addtolist(guibut,listIn):
     if guibut and (guibut not in listIn):
         listIn.append(guibut)
+
+'''
+#For debugging settingGUI
+defaultALL = ['http://calendar.utexas.edu/calendar.xml', 'http://calendar.mit.edu/calendar.xml', 'http://events.umich.edu/day/rss', 'http://events.umich.edu/week/rss']        
+a = setGUI(defaultALL)    
+print('INITIAL SETTINGS FROM GUI:')
+print(a.doCalendar + ' Calendar', a.foodToSearch, a.URLlist, sep = '\n')
+'''
