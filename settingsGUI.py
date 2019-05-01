d38e7cbfe8264f89b36f455ad370af5a7b08c212
georginad <42850648+georginad@users.noreply.github.com>
2019-04-30 22:09:40 -0500

    Added functionality, included picture

--- settingsGUI.py
+++ settingsGUI.py
@@ -1,39 +1,57 @@
 import tkinter as tk
 from tkinter import simpledialog
 import Feeder
+from PIL import ImageTk, Image
 
 class setGUI:
     
-    def __init__(self,defaults = []):
+    def __init__(self,defaults = [],fooddefaults = []):
         self.foodToSearch = ['food']
         self.doCalendar = 'YES'        
         self.URLlist = ['http://calendar.utexas.edu/calendar.xml']
         self.FIXEDdefaults = defaults
         self.defaults = defaults.copy()
+        self.FIXEDFOODdefaults = fooddefaults
+        self.fooddefaults = fooddefaults.copy()
+        
+        boldbg = 'red' #'#BF5700' #"red"
+        txtfg = 'red' #'#BF5700' #"red"
+        UTcol = '#BF5700'
         
         self.launch = tk.Tk()
         self.launch.title("Initial Settings")
+        
+        #Set dotnom image in GUI
+        img = ImageTk.PhotoImage(Image.open("dotnom.png"))
+        imgset = tk.Label(self.launch,image = img)
+        imgset.grid(row = 0, column = 0, rowspan = 14)
+        #Put .nom in 
+        tk.Label(self.launch,
+                 text = ".nom",
+                 fg = "white",
+                 bg = UTcol,
+                 font = ('Algerian','24','bold')).grid(row = 0, column = 0)         
         
         #Ask if calendar should be made
         tk.Label(self.launch,
                  text = "CURRENT CALENDAR OUTPUT:",
                  fg = "white",
-                 bg = "red",
-                 font = ('Georgia','24','bold')).grid(row = 0, column = 0)        
+                 bg = boldbg,
+                 font = ('Georgia','24','bold')).grid(row = 0, column = 1)        
         self.calendar = tk.Label(self.launch,
                  text = '\nYES Calendar ==> .ics file will be saved\n',
-                 fg = "red",
+                 fg = txtfg,
                  font = ('Georgia','14','italic'))
-        self.calendar.grid(row = 1, column = 0)
+        self.calendar.grid(row = 1, column = 1)
         
-        tk.Button(self.launch,text = "CHANGE CALENDAR OPTION", command = self.tkYesNo).grid(row = 2, column = 0)
+        tk.Button(self.launch,text = "CHANGE CALENDAR OPTION", command = self.tkYesNo).grid(row = 2, column = 1)
         
         #Ask to add food options, and display list of options
         tk.Label(self.launch,
                  text = "CURRENT FOOD OPTIONS:",
                  fg = "white",
-                 bg = "red",
-                 font = ('Georgia','24','bold')).grid(row = 4, column = 0)
+                 bg = boldbg,
+                 font = ('Georgia','24','bold')).grid(row = 4, column = 1)
         
         scrollfood = tk.Scrollbar(self.launch)
         scrollfood.grid(row = 5, column = 50, sticky='ns')
@@ -45,8 +63,8 @@ class setGUI:
         for fd in self.foodToSearch:
             self.foodlist.insert(tk.END,fd)
         
-        self.foodlist.config(fg = "red", font = ('Georgia','14','italic'))
-        self.foodlist.grid(row = 5, column = 0)
+        self.foodlist.config(fg = txtfg, font = ('Georgia','14','italic'))
+        self.foodlist.grid(row = 5, column = 1)
         
         scrollfood.config(command=self.foodlist.yview)
 
@@ -56,13 +74,26 @@ class setGUI:
                  font = ('Georgia','12')).grid(row = 11, column = 0)
                
         tk.Button(self.launch,text = "ADD FOOD",command = self.dialfood).grid(row = 12, column = 0)
+
+        tk.Label(self.launch,
+                 text = "\nClear all food options:\n",
+                 fg = "black",
+                 font = ('Georgia','12')).grid(row = 11, column = 2)
+               
+        tk.Button(self.launch,text = "CLEAR FOOD LIST",command = self.clearfood).grid(row = 12, column = 2)
+
+        tk.Label(self.launch,
+                 text = "\nOr select from loaded defaults:\n",
+                 fg = "black",
+                 font = ('Georgia','12')).grid(row=11,column = 1)
+        tk.Button(self.launch,text = "LOAD DEFAULT FOODS",command = self.defaultfoods).grid(row = 12, column = 1)
         
         #Ask to add the url links and include a check to make sure that the link contains a valid rss feed (.xml or /rss)
         tk.Label(self.launch,
                  text = "CURRENT URL OPTIONS:",
                  fg = "white",
-                 bg = "red",
-                 font = ('Georgia','24','bold')).grid(row = 13, column = 0) 
+                 bg = boldbg,
+                 font = ('Georgia','24','bold')).grid(row = 13, column = 1) 
         
         scrollurl = tk.Scrollbar(self.launch)
         scrollurl.grid(row = 14, column = 65, sticky='ns')
@@ -74,8 +105,8 @@ class setGUI:
         for ul in self.URLlist:
             self.url.insert(tk.END,ul)
         
-        self.url.config(fg = "red", font = ('Georgia','14','italic'))
-        self.url.grid(row = 14, column = 0)
+        self.url.config(fg = txtfg, font = ('Georgia','14','italic'))
+        self.url.grid(row = 14, column = 1)
         
         scrollfood.config(command=self.url.yview)
         
@@ -85,14 +116,24 @@ class setGUI:
                  font = ('Georgia','12')).grid(row = 20, column = 0)
         
         tk.Button(self.launch,text = "ADD URL",command = self.dialurl).grid(row = 21, column = 0)
+ 
+        tk.Label(self.launch,
+                 text = "\nClear all url options:\n",
+                 fg = "black",
+                 font = ('Georgia','12')).grid(row = 20, column = 2)
+        
+        tk.Button(self.launch,text = "CLEAR URL LIST",command = self.clearurl).grid(row = 21, column = 2)
+        
         tk.Label(self.launch,
                  text = "\nOr select from loaded defaults:\n",
                  fg = "black",
-                 font = ('Georgia','12')).grid(row=22,column = 0)
-        tk.Button(self.launch,text = "LOAD DEFAULT URLS",command = self.defaulturls).grid(row = 23, column = 0)
+                 font = ('Georgia','12')).grid(row=20,column = 1)
+        tk.Button(self.launch,text = "LOAD DEFAULT URLS",command = self.defaulturls).grid(row = 21, column = 1)
+        
+        tk.Label(self.launch,text = "\n").grid(row=22,column = 0) #blank space       
         
         #Exit when done
-        tk.Button(self.launch,text = "DONE",command = self.done).grid(row = 24, column = 0)
+        tk.Button(self.launch,text = "DONE",bg = "blue",fg = "white",font = ('Georgia','12','bold'),command = self.done).grid(row = 23, column = 1)
         
         self.launch.mainloop()
         
@@ -120,7 +161,41 @@ class setGUI:
         torem = simpledialog.askinteger("delete url","Please Enter index of URL to remove.\nIndices are given in the LOADED DEFAULTS list by the URL.",parent=self.deflt,minvalue=0,maxvalue=len(self.defaults)-1) 
         if torem in range(len(self.defaults)): del(self.defaults[torem])
         self.defList['text'] = listTostr(self.defaults,'\n',item=1)
-                
+
+    def dialfdel(self):
+        torem = simpledialog.askstring("delete food","Please Enter name of FOOD to remove.",parent=self.fdeflt) 
+        torem = torem.strip()
+        if torem in self.fooddefaults:
+            LIST = self.fdeflist.get(0,tk.END)
+            idx = LIST.index(torem)
+            self.fdeflist.delete(idx)
+            
+            del(self.fooddefaults[idx])
+    
+    def clearfood(self):
+        self.foodToSearch = []
+        LIST = self.foodlist.get(0,tk.END)
+        Rerun = True
+        while Rerun:
+            for food in LIST:
+                self.foodlist.delete(LIST.index(food))
+            if self.foodlist.get(0,tk.END): 
+                Rerun = True
+                LIST = self.foodlist.get(0,tk.END)
+            else: Rerun = False
+            
+    def clearurl(self):
+        self.URLlist = []
+        LIST = self.url.get(0,tk.END)
+        Rerun = True
+        while Rerun:
+            for url in LIST:
+                self.url.delete(LIST.index(url))
+            if self.url.get(0,tk.END): 
+                Rerun = True
+                LIST = self.url.get(0,tk.END)
+            else: Rerun = False
+            
     def YESDOCalendar(self):
         self.calendar['text'] = '\nYES Calendar ==> .ics file will be saved\n'
         self.doCalendar = 'YES'
@@ -138,11 +213,16 @@ class setGUI:
         self.launch.destroy()
         
     def loaddef(self):
-        self.URLlist = []
         for url in self.defaults:
             addtolist(url,self.URLlist)
-            self.url.insert(tk.END, url) 
+            self.url.insert(tk.END, url)
         self.deflt.destroy()
+        
+    def loadfdef(self):
+        for url in self.fooddefaults:
+            addtolist(url,self.foodToSearch)
+            self.foodlist.insert(tk.END, url)
+        self.fdeflt.destroy()   
         
     def tkYesNo(self):
         #simple yes no dialogue box for calendar
@@ -184,22 +264,55 @@ class setGUI:
         tk.Button(self.deflt,text = "LOAD DEFAULTS", command = self.loaddef).pack()
         tk.Button(self.deflt,text = "DELETE URL FROM DEFAULTS", command = self.dialdel).pack()
         self.deflt.mainloop()
+
+    def defaultfoods(self):
+        #show all loaded defaults and let users choose which to delete/load
+        self.fooddefaults = self.FIXEDFOODdefaults.copy()
+        self.fdeflt = tk.Tk()
+        self.fdeflt.title("default FOODS")
+        tk.Label(self.fdeflt,
+                 text = "LOADED DEFAULTS:",
+                 fg = "white",
+                 bg = "black",
+                 font = ('Georgia','18','bold')).grid(row = 0, column = 0)
+        
+        scrollfood = tk.Scrollbar(self.fdeflt)
+        scrollfood.grid(row = 1, column = 30, sticky='ns')
+       
+        self.fdeflist = tk.Listbox(self.fdeflt,
+                                   width =  30, height = 22,
+                                   yscrollcommand = scrollfood.set)
+
+        #initial foodlist
+        for fd in self.fooddefaults:
+            self.fdeflist.insert(tk.END,fd)
+        
+        self.fdeflist.config(fg = "black", font = ('Georgia','14','italic'))
+        self.fdeflist.grid(row = 1, column = 0, sticky='ns')
+
+        scrollfood.config(command=self.fdeflist.yview)
+        
+        tk.Button(self.fdeflt,text = "LOAD DEFAULTS", command = self.loadfdef).grid(row = 23, column = 0)
+        tk.Button(self.fdeflt,text = "DELETE FOOD FROM DEFAULTS", command = self.dialfdel).grid(row = 24, column = 0)
+        self.fdeflt.mainloop()
         
 def listTostr(foodIn,sep,item=0):
     #item sets if the list string should be itemized
     foods = foodIn.copy()
-    outstr = str(foods[0])
-    ctr = 0
-    if item:
-        outstr += ' ['+str(ctr)+']'
-        ctr += 1
+    outstr = ''
+    if foods:
+        outstr = str(foods[0])
+        ctr = 0
+        if item:
+            outstr += ' ['+str(ctr)+']'
+            ctr += 1
+            
+        foods.pop(0)
         
-    foods.pop(0)
-    
-    for food in foods:
-        outstr += sep + str(food)
-        if item: outstr += ' ['+str(ctr)+']' 
-        ctr += 1
+        for food in foods:
+            outstr += sep + str(food)
+            if item: outstr += ' ['+str(ctr)+']' 
+            ctr += 1
         
     return outstr
 
@@ -207,10 +320,10 @@ def addtolist(guibut,listIn):
     if guibut and (guibut not in listIn):
         listIn.append(guibut)
 
-'''
+
 #For debugging settingGUI
-defaultALL = ['http://calendar.utexas.edu/calendar.xml', 'http://calendar.mit.edu/calendar.xml', 'http://events.umich.edu/day/rss', 'http://events.umich.edu/week/rss']        
-a = setGUI(defaultALL)    
+defaultALL = ['http://calendar.utexas.edu/calendar.xml', 'http://calendar.mit.edu/calendar.xml', 'http://events.umich.edu/day/rss', 'http://events.umich.edu/week/rss','https://law.utexas.edu/calendar/feed/rss/','https://music.utexas.edu/events/calendar.xml']        
+defaultFOOD = ['food', 'pizza', 'chinese', 'burgers', 'chicken', 'fries', 'rice', 'refreshments', 'cookies', 'sushi', 'sandwiches', 'coffee', 'dougnuts', 'snacks', 'beer', 'cupcakes', 'brownies', 'tacos', 'breakfast', 'lunch', 'dinner', 'luncheon', 'hotdog', 'beans','chocolate']
+a = setGUI(defaultALL,defaultFOOD)    
 print('INITIAL SETTINGS FROM GUI:')
 print(a.doCalendar + ' Calendar', a.foodToSearch, a.URLlist, sep = '\n')
-'''
